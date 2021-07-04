f69b80bf560531905b6779a8f705f7dbcf054966
jana <rathan.kparam@gmail.com>
2021-07-04 12:13:27 +0800

    Refactor

--- next-base/pages/App.js
+++ next-base/pages/App.js
@@ -5,11 +5,10 @@ const App = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(async () => {
-    console.log('useEffect', new Date());
+    console.log('useEffect', new Date().toISOString());
     const res = await fetch("https://api.github.com/users/hadley/orgs");
     const jsonRes = await res.json();
     setPosts(jsonRes);
-    console.log(`posts ${jsonRes}`);
   }, [])
 
   return (
--- next-base/pages/index.js
+++ next-base/pages/index.js
@@ -1,12 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import App from './App'
 
 export default function Home() {
   return (
     <div>
-      App here to access api
+      App here to access a public api
       <App></App>
     </div>
   )
