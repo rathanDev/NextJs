84371dfd4942d5eec89584f59741c4a67cc12bef
jana <rathan.kparam@gmail.com>
2021-07-04 12:02:45 +0800

    Add component in next app

---
+++ next-base/pages/App.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+const App = () => {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(async () => {
+    console.log('useEffect', new Date());
+    const res = await fetch("https://api.github.com/users/hadley/orgs");
+    const jsonRes = await res.json();
+    setPosts(jsonRes);
+    console.log(`posts ${jsonRes}`);
+  }, [])
+
+  return (
+    <div className="App">
+      {posts.map(p => (
+        <li key={p.id}>{p.login}</li>
+      ))}
+    </div>
+  );
+}
+
+export default App;
