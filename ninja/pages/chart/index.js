88b2cb2e3b1f7b95f134d62948946a67e7d3f42b
jana <rathan.kparam@gmail.com>
2021-07-04 19:26:39 +0800

    Persever

--- ninja/pages/chart/index.js
+++ ninja/pages/chart/index.js
@@ -1,28 +1,51 @@
-const ChartComponent = ({ posts }) => {
+const ChartComponent = ({ list }) => {
 
-    console.log(`Inside ChartComponent ${posts}`);
+    console.log(`Inside ChartComponent `); // ${list}
+
+    // list.forEach((todos, h) => {
+    //     todos.forEach((todo, i) => {
+    //         console.log(todo.title, h, i);
+    //     })
+    // });
 
     return (
         <div>
             App <br />
-            {posts.map(p => (
-                <li key={p.id}>{p.login}</li>
-            ))}
+
+            {list.map((subArray, index) => {
+                return (
+                    <div key={index}>
+                        {subArray.map((subitem, i) => {
+                            return ((index) && (
+                                <ul key={index * i}>
+                                    <li>{subitem.title}</li>
+                                </ul>
+                            ));
+                        })}
+                    </div>
+                );
+            })}
+
         </div>
     );
 }
 
 export async function getStaticProps() {
 
-    const res = await fetch("https://api.github.com/users/hadley/orgs");
-    const posts = await res.json();
-    console.log(`posts ${posts}`);
+    console.log(`Inside getStaticProps`, new Date().toISOString());
 
-    // localStorage.setItem('posts', posts);
+    const url = "https://jsonplaceholder.typicode.com/todos";
+    const list = [];
+    for (let i = 0; i < 100; i++) {
+        const res = await fetch(url);
+        const posts = await res.json();
+        list.push(posts);
+    }
+    // console.log(`list ${list}`);
 
     return {
         props: {
-            posts,
+            list,
         },
     }
 }
